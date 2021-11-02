b3ad96c1d9cb7789270e2c61caf0075eedc092f2
Tianyi Jian <jian0092@algonquinlive.com>
2021-11-02 17:19:46 -0400

    Initialize DB Done

---
+++ Lab4/Models/CommunityMembership.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class CommunityMembership
+    {
+        /*
+         * student id
+         */
+        public int StudentId { get; set; }
+        /*
+         * Community id
+         */
+        public string CommunityId { get; set; }
+
+        public Student Student { get; set; }
+
+        public Community Community { get; set; }
+    }
+}
---
+++ Lab4/Models/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Student
+    {
+        /*
+         * public integer Id for students
+         */
+        public int Id { get; set; }
+
+        /*
+         * Last name of a student, required, with strengthlength 50 and minimum length 1
+         */
+        [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        /*
+         * First name of a student, required, with strengthlength 50 and minimum length
+         */
+        [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        /*
+         * DateTime EnrollmentDate
+         */
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        /*
+         * calculated field from: LastName + “, “ + FirstName
+         */
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        public ICollection<CommunityMembership> CommunityMemberships { get; set; }
+    }
+}
