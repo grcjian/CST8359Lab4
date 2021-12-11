69121c3eb2767a1cb40cab33c644c3b1fecb55ca
Tianyi Jian <jian0092@algonquinlive.com>
2021-12-10 23:42:42 -0500

    Last Version

--- Lab4/Startup.cs
+++ Lab4/Startup.cs
@@ -28,7 +28,7 @@ namespace Lab4
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = Configuration.GetConnectionString("LocalDBConnection");
+            var connection = Configuration.GetConnectionString("DefaultConnection1");
             services.AddDbContext<SchoolCommunityContext>(options => options.UseSqlServer(connection));
             //services.AddDbContext<AnswerImageDataContext>(options => options.UseSqlServer(connection));
             var blobConnection = Configuration.GetConnectionString("AzureBlobStorage");
